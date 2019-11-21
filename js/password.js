c4ceb57a1cf72b05a5ded1cd1b882e0687244968
Will Coan <wcoan300@gmail.com>
2019-11-21 12:51:32 -0500

    added images and copy to clipboard

--- js/password.js
+++ js/password.js
@@ -68,3 +68,19 @@ function password() {
     }
 
 }
+
+
+function copyClip() {
+   
+    var copyText = document.getElementById("myInput");
+  
+  
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+    
+    document.execCommand("copy");
+  
+   
+    alert("Copied the text: " + copyText.value);
+  }
